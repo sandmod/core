1a20b5544e5e5a74876e54e45e2b951ee91cc05c
Game4Freak <25063129+Game4Freak@users.noreply.github.com>
2023-09-17 01:41:53 +0200

    Added ProvideSingle

--- code/Provider/ProviderFactory.cs
+++ code/Provider/ProviderFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Sandbox;
 using Sandmod.Core.Logger;
@@ -17,8 +18,8 @@ public static class ProviderFactory
     /// <returns>the created instances</returns>
     public static IReadOnlyList<T> Provide<T>() where T : class
     {
+        var log = CreateLogger<T>();
         var typeName = typeof(T).Name;
-        var log = new SandmodLogger($"Provider/{typeof(T).Name}");
         log.Info($"Providing instances for {typeName} ...");
 
         var types = TypeLibrary.GetTypes<T>().ToList();
@@ -37,14 +38,14 @@ public static class ProviderFactory
         var defaultTypes = types
             .Select(type => new KeyValuePair<TypeDescription, DefaultProviderAttribute>(type,
                 TypeLibrary.GetAttribute<DefaultProviderAttribute>(type.TargetType))).Where(pair => pair.Value != null)
-            .GroupBy(pair => pair.Value.Priority).OrderBy(group => group.Key).ToList();
+            .GroupBy(pair => pair.Value.Priority).OrderByDescending(group => group.Key).ToList();
         if (defaultTypes.Any())
         {
             var defaultPriority = defaultTypes.First();
             if (defaultPriority.Count() > 1)
             {
                 log.Warning(
-                    $"Multiple default types found for the highest priority, using the first found: {string.Join(", ", defaultPriority.Select(pair => pair.Key.FullName))}");
+                    $"Multiple default types found for the highest priority, using the first found: {defaultPriority.First().Key.FullName}");
             }
 
             var defaultType = defaultPriority.First();
@@ -55,5 +56,37 @@ public static class ProviderFactory
         // Should not be reachable, unless not used properly
         log.Warning("Nothing found to provide. Normally there should be at least an internal default type");
         return new List<T>();
+    }
+
+    /// <summary>
+    /// Creates a instance of the provided type <b><typeparamref name="T"/></b> based on the <see cref="ProviderAttribute"/> and <see cref="DefaultProviderAttribute"/>.
+    /// </summary>
+    /// <typeparam name="T">The type to provide</typeparam>
+    /// <returns>the created instance</returns>
+    public static T ProvideSingle<T>() where T : class
+    {
+        var log = CreateLogger<T>();
+        var provided = Provide<T>();
+        if (provided.Count <= 0)
+        {
+            var typeName = typeof(T).Name;
+            log.Error($"No instance for {typeName} provided but one is required");
+            return null;
+        }
+
+        var result = provided.First();
+
+        if (provided.Count > 1)
+        {
+            log.Warning(
+                $"{provided.Count} instances provided, using only the first found: {result.GetType().FullName}");
+        }
+
+        return result;
+    }
+
+    private static SandmodLogger CreateLogger<T>()
+    {
+        return new SandmodLogger($"Provider/{typeof(T).Name}");
     }
 }
